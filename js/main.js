fa47429ba864c2520b442a755f144e5d36f9864e
damirGDR <rmcalypse13@mail.ru>
2016-08-14 19:13:04 +0300

    Go!

---
+++ js/main.js
@@ -0,0 +1,142 @@
+
+$( document ).ready(function() {
+    generate();
+});
+
+//var time = [1, 3, 6, 9, 14, 19, 24, 12, 45];
+
+function p(el){
+    console.log(el);
+}
+
+var numbers = shuffle([1,2,3,4,5, 6, 7, 8, 9]),
+numbers = [1,2,3,4,5, 6, 7, 8, 9],
+    count = numbers.length,
+    width = 600;
+    treeManager = new TreeManager(width, count);
+
+function getNumbers(){
+
+    var numbers = [];
+
+    for(var i=0;i<count;i++){
+        numbers.push(Math.floor((Math.random() * 10) + 1));
+    }
+    return shuffle(numbers);
+}
+
+function shuffle(o) {
+	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i),
+	    x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+};
+
+function generate(){
+
+    var elemTmpl = $.templates("#Elem"),
+		list = $('.list');
+
+    $(numbers).each(function(index, number){
+        setTimeout(function (){
+            list.append(
+                elemTmpl.render({val:number})
+            );
+        }, 200*(index));
+    });
+}
+
+function prebuild(){
+	var copyTmpl = $.templates("#copyElem"),
+		treeList = $('.list-copy');
+	
+	$(".list-item").each(
+        function(index, el){
+            var $el = $(el);
+
+            setTimeout(function (){
+				treeList.append(copyTmpl.render({
+					val:$el.find('p').text()
+					}));
+
+            }, 50*index);
+        }
+    );
+
+    setTimeout(build, 1000);
+}
+
+function summy(arr){
+    var res = [arr[0]];
+
+    for(var i=1;i<arr.length;i++){
+        res.push(
+            [res[i-1][0]+arr[i][0],
+            res[i-1][1]+arr[i][1],
+            arr[i][2]]);
+    }
+    return res
+}
+
+
+
+function build(){
+
+    var items = $(".copy-list-item"),
+        root = $("#r"),
+        rX = root.offset().left,
+        rY = root.offset().top-50;
+
+    items.removeClass('slideUpRetourn');
+
+    items.each(
+        function(index, el){
+            var $el = $(el),
+                elX = $el.offset().left,
+                elY = $el.offset().top;
+
+            setTimeout(function(){
+
+                var coords = (treeManager.add($el.attr('value')));
+                coords = summy([[rX-elX, rY-elY]].concat(coords));
+
+                $(coords).each(
+                    function(j, coord){
+                        setTimeout(
+                            function(){
+                                $el.css('-webkit-transform', "translate("+
+                                (coord[0])+"px,"+
+                                (coord[1])+"px)");
+                                $el.find('span').text(coord[2]);
+//                            }, 1000*j);
+                            }, 100*j);
+                    }
+                )
+            }, 1000*index);
+        }
+    );
+}
+
+function sort(){
+    var result = treeManager.sortIter(),
+        elemTmpl = $.templates("#Elem"),
+		sList = $('.sorted');
+
+    $(result).each(
+        function(index, res){
+
+            setTimeout(function(){
+
+            var i = $('.copy-list-item').filter(function(index){
+
+                    return $( this ).attr( "value" ) ==res[0];
+               })
+            i.css('background-color', 'transparent');
+            for(var i=0; i<res[1]; i++){
+                sList.append(elemTmpl.render({
+					val:res[0]
+					}));
+            }
+            }, index*1000);
+        });
+}
+
---
+++ js/tree.js
@@ -0,0 +1,115 @@
+
+function Node(val, level, pos){
+	this.val = val;
+	this.level = level;
+	this.position = pos;
+	this.count = 1;
+	
+	this.l = null;
+	this.r = null;
+}
+
+function Tree(width, levels){
+	this.root = null;
+	this.ranges = this.getRanges(width, levels);
+}
+
+Tree.prototype.getRanges = function(width, levels){
+
+	var ranges = [];
+
+	for(var i=0;i<levels;i++){
+		ranges[i+1] = width/((i+1)*2)
+	}
+	return ranges;
+}
+
+var p = console.log
+
+Tree.prototype.addOut = function(nodeOut, valOut) {
+
+	var Y = 50;
+		coords = [],
+		tree = this;
+
+	function addInn(node, val){
+		if(node == null){
+			tree.root = new Node(val, 1, 1);
+			coords.push([0, Y, 1]);
+
+		} else if(node.val == val){
+			node.count += 1;
+			coords.push([0, Y, node.count]);
+
+		} else if(node.val > val){
+		 	var X = tree.ranges[node.level+1];
+
+		 	coords.push([-X, Y, 1]);
+
+			if(node.l == null){
+				node.l = new Node(val, node.level+1, node.position*2-1);
+				coords.push([0, Y, 1]);
+
+			} else {
+				addInn(node.l, val);
+			}
+		} else {
+			var X = tree.ranges[node.level+1];
+			coords.push([X, Y]);
+
+			if(node.r == null){
+				node.r = new Node(val, node.level+1, node.position*2+1);
+
+				coords.push([0, Y, 1]);
+			} else {
+				addInn(node.r, val);
+			}
+		}
+	}
+	addInn(nodeOut, valOut);
+
+	return coords
+}
+
+
+Tree.prototype.sortOut = function(nodeOut){
+	var items = [];
+
+	function sortInn(node){
+		if(node.l != null){
+			sortInn(node.l);
+		}
+		items.push(node);
+		if(node.r != null){
+			sortInn(node.r);
+		}
+	}
+
+	sortInn(nodeOut);
+
+	return items;
+}
+
+
+function TreeManager(width, levels){
+	var items = [];
+
+	this.tree = new Tree(width, levels);
+}
+
+
+TreeManager.prototype.add = function(val){
+	return this.tree.addOut(this.tree.root, val);
+}
+
+TreeManager.prototype.sortIter = function(val){
+
+	var nodes =  this.tree.sortOut(this.tree.root),
+		result = [];
+
+	$(nodes).each(
+        function(index, node){
+            result.push([node.val, node.count]);
+        })
+    return result
+}
